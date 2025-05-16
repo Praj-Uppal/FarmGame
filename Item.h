2baf98917b8a779d6e8fdb37e0b11fe3ba2e1d74
Praj-Uppal <a1925661@adelaide.edu.au>
2025-05-16 02:12:11 +0000

    Defined Item class according to plan

---
+++ Item.h
@@ -0,0 +1,34 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Item
+{
+protected:
+    // Define Item Attributes including sell cost, buy cost, if its plantable and its name
+    int sellCost, buyCost;
+    bool plantable;
+    string name;
+
+public:
+    // Default and normal constructor
+    Item(int sell, int buy, string name);
+    Item();
+
+    // Public Method to get variable values. Const to ensure they dont change attribute values.
+    int getSellCost() const;
+    int getBuyCost() const;
+    string getName() const;
+    bool getPlantable() const;
+
+    // Public Methods to set variable values.
+    void setSellCost(int sell);
+    void setBuyCost(int buy);
+    void setName(string name);
+    void setPlantable(bool status);
+};
+
+#endif
