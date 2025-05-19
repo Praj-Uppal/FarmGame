490c706bfb4712eae1d7f9906d9a91baaf14c6a9
Praj-Uppal <a1925661@adelaide.edu.au>
2025-05-19 02:27:22 +0000

    Added functions to remove or add single items

--- Item.cpp
+++ Item.cpp
@@ -1,7 +1,9 @@
 #include "Item.h"
 
-// Define constructor. First value is buy cost, followed by sell cost, and lastly the name of the item
-Item::Item(int buy, int sell, string name) : buyCost(buy), sellCost(sell), name(name) {};
+// Define constructor. First value is buy cost, followed by sell cost, and
+// lastly the name of the item
+Item::Item(int buy, int sell, string name)
+    : buyCost(buy), sellCost(sell), name(name) {};
 // Define Default; let buy cost = sell cost = 0, and name be blank;
 Item::Item() : Item(0, 0, "") {};
 
@@ -11,7 +13,8 @@ int Item::getSellCost() const { return sellCost; }
 string Item::getName() const { return name; }
 bool Item::getPlantable() const { return plantable; }
 
-// Define Set methods. Simply sets the corresponding variable to the provided value.
+// Define Set methods. Simply sets the corresponding variable to the provided
+// value.
 void Item::setBuyCost(int buy) { buyCost = buy; }
 void Item::setSellCost(int sell) { sellCost = sell; }
 void Item::setName(string name) { this->name = name; }
