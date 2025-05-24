11015d98947c66d6adcdf1481e08a8d89b195693
Spagett1 <laar@tutanota.com>
2025-05-24 10:42:33 +0930

    Fixing the plant pointer handling

--- src/Player.cpp
+++ src/Player.cpp
@@ -46,11 +46,11 @@ void Player::plant(Item* item,FarmPlot* farmplot) {
   
   //Define general plant pointer 
     Plant* p1;
-    if (item == Potato){
+    if (item->getName() == "Potato"){
       //If item is potato, let p1 be a new potato plant
     p1 = new PotatoPlant(this->position);
     }
-    else if (item == Carrot) {
+    else if (item->getName() == "Carrot") {
       //if item is carrot, let p1 be a new carrot plant
     p1 = new CarrotPlant(this->position);
     }
