1de69e1ec92042ebd99934b16b916e58c2c2f9d5
Praj-Uppal <a1925661@adelaide.edu.au>
2025-05-24 10:35:59 +0930

    Deleted extern item references from Allincludes

--- src/Allincludes.h
+++ src/Allincludes.h
@@ -6,29 +6,11 @@
 #include <map>
 #include <tuple>
 #include <vector>
-#include<ncurses.h>
+#include <ncurses.h>
 
 using namespace std;
 // Defining a type for coordinate
 typedef tuple<int, int> coord;
 
-
-// Declare Item attributes. Item(Sell Price, Buy price, Name)
-//Also Declaring a pointer pointing to that specific item.
-#include"Item.h"
-extern Item Carrotitem;
-extern Item* Carrot;
-
-extern Item Potatoitem;
-extern Item* Potato;
-
-// Animals will need to have 2 items, one that is produced when
-// harvested, and one which is used to create entity. For entity item make
-// sure buy price and sell price are same to prevent infinite money glitches
-extern Item cowCattleitem;
-extern Item* cattle;
-// Cant purchase steak from shop so no point to Buyprice
-extern Item Steakitem;
-extern Item* Steak;
 
 #endif
