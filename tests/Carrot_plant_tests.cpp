199b86b27cec96eb1326bfb5e24fc1642ae993fd
Praj-Uppal <a1925661@adelaide.edu.au>
2025-05-25 20:50:52 +0930

    Added testing for harvest and advance day

--- tests/Carrot_plant_tests.cpp
+++ tests/Carrot_plant_tests.cpp
@@ -1,9 +1,8 @@
 #include "CarrotPlant.h"
 
-
 void plant_constructor_test() {
   // Create plant
-  Plant* p1 =new CarrotPlant(make_tuple(1, 1));
+  Plant* p1 = new CarrotPlant(make_tuple(1, 1));
 
   if (p1->getPosistion() != make_tuple(1, 1)) {
     cout << "Plant position not initialised correctly! Plant is not at (1,1)";
@@ -30,6 +29,7 @@ void plant_constructor_test() {
     return;
   }
   cout << "All plant constructor tests passed!" << endl;
+  delete p1;
 }
 
 void plant_set_method_tests() {
@@ -57,7 +57,7 @@ void plant_set_method_tests() {
     cout << "Set method for position has failed!" << endl;
     return;
   }
-
+  delete p1;
   cout << "All set methods functional! Tests passed!" << endl;
 }
 
@@ -66,25 +66,83 @@ void plant_waterMethods_tests() {
   Plant* p1 = new CarrotPlant(make_tuple(1, 1));
   // Water plant
   p1->waterPlant();
-  //Check if watered 
+  // Check if watered
   if (!p1->isWatered()) {
     cout << "Watering plant test failed!" << endl;
     return;
   }
-  //Dry plan
+  // Dry plan
   p1->dryPlant();
-  //Check if dried
+  // Check if dried
   if (p1->isWatered()) {
     cout << "Drying Plant Test Failed!" << endl;
     return;
   }
 
   cout << "All Watering methods tests passed!" << endl;
+  delete p1;
+}
+
+void plant_advanceDay_test() {
+  // Create plant
+  Plant* p1 = new CarrotPlant(make_tuple(1, 1));
+  // Water plant
+  p1->waterPlant();
+  // call advance day
+  p1->advanceDay();
+  // Check
+  if (p1->getCaredForDays() !=
+      0) {  // 0 because it should progress in growth stage
+    cout << "Plant cared for days is incorrect!" << endl;
+    return;
+  }
+  if (p1->isWatered()) {
+    cout << "Plant was not dried!" << endl;
+    return;
+  }
+  // Also check if growth stage progressed
+  if (p1->getGrowthStage() != 1) {
+    cout << "Plant did not grow!" << endl;
+    return;
+  }
+
+  cout << "All advance day tests passed!" << endl;
+  delete p1;
+}
+
+void plant_harvest_test() {
+  // Create plant
+  Plant* p1 = new CarrotPlant(make_tuple(1, 1));
+  vector<tuple<Item*, int>> result = p1->harvest();
+  // Check if its not empty
+  if (!result.empty()) {
+    cout << "Harvest triggered incorrectly!" << endl;
+    return;
+  }
+  // Make mature
+  p1->setGrowthStage(3);
+  // Call harvest
+  result = p1->harvest();
+  // Create Item
+  Item* Carrot = new Item(2, 1, "Carrot");
+  // Define expected result
+  tuple<Item*, int> Expectedresult = make_tuple(Carrot, 2);
+  // Check if actual result matches expected result;
+  if (std::get<1>(result.at(0)) != std::get<1>(Expectedresult)) {
+    cout << "Harvest results incorrect!" << endl;
+    return;
+  }
+  cout << "All harvest tests passed!" << endl;
+  // Delete
+  delete Carrot;
+  delete p1;
 }
 
 int main(void) {
-    //Run Tests
-    plant_constructor_test();
-    plant_set_method_tests();
-    plant_waterMethods_tests();
+  // Run Tests
+  plant_constructor_test();
+  plant_set_method_tests();
+  plant_waterMethods_tests();
+  plant_advanceDay_test();
+  plant_harvest_test();
 }
--- tests/Potato_Plant_tests.cpp
+++ tests/Potato_Plant_tests.cpp
@@ -1,9 +1,8 @@
 #include "PotatoPlant.h"
 
-
 void plant_constructor_test() {
   // Create plant
-  Plant* p1 =new PotatoPlant(make_tuple(1, 1));
+  Plant* p1 = new PotatoPlant(make_tuple(1, 1));
 
   if (p1->getPosistion() != make_tuple(1, 1)) {
     cout << "Plant position not initialised correctly! Plant is not at (1,1)";
@@ -30,6 +29,7 @@ void plant_constructor_test() {
     return;
   }
   cout << "All plant constructor tests passed!" << endl;
+  delete p1;
 }
 
 void plant_set_method_tests() {
@@ -59,6 +59,7 @@ void plant_set_method_tests() {
   }
 
   cout << "All set methods functional! Tests passed!" << endl;
+  delete p1;
 }
 
 void plant_waterMethods_tests() {
@@ -66,25 +67,83 @@ void plant_waterMethods_tests() {
   Plant* p1 = new PotatoPlant(make_tuple(1, 1));
   // Water plant
   p1->waterPlant();
-  //Check if watered 
+  // Check if watered
   if (!p1->isWatered()) {
     cout << "Watering plant test failed!" << endl;
     return;
   }
-  //Dry plan
+  // Dry plan
   p1->dryPlant();
-  //Check if dried
+  // Check if dried
   if (p1->isWatered()) {
     cout << "Drying Plant Test Failed!" << endl;
     return;
   }
 
   cout << "All Watering methods tests passed!" << endl;
+  delete p1;
+}
+
+void plant_advanceDay_test() {
+  // Create plant
+  Plant* p1 = new PotatoPlant(make_tuple(1, 1));
+  // Water plant
+  p1->waterPlant();
+  // call advance day
+  p1->advanceDay();
+  // Check
+  if (p1->getCaredForDays() != 1) {  // 1 because it needs 6 care days
+    cout << "Plant cared for days is incorrect!" << endl;
+    return;
+  }
+  if (p1->isWatered()) {
+    cout << "Plant was not dried!"
+         << endl;  // Plants should dry when day advance
+    return;
+  }
+  // Also check if growth stage progressed
+  if (p1->getGrowthStage() != 0) {
+    cout << "Plant did grew!" << endl;
+    return;
+  }
+
+  cout << "All advance day tests passed!" << endl;
+  delete p1;
+}
+
+void plant_harvest_test() {
+  // Create plant
+  Plant* p1 = new PotatoPlant(make_tuple(1, 1));
+  vector<tuple<Item*, int>> result = p1->harvest();
+  // Check if its not empty
+  if (!result.empty()) {
+    cout << "Harvest triggered incorrectly!" << endl;
+    return;
+  }
+  // Make mature
+  p1->setGrowthStage(3);
+  // Call harvest
+  result = p1->harvest();
+  // Define item
+  Item* Potato = new Item(4, 2, "Potato");
+  // Define Expected result
+  tuple<Item*, int> Expectedresult = make_tuple(Potato, 4);
+  // Check if actual result count matches expected result count
+  if (std::get<1>(result.at(0)) != std::get<1>(Expectedresult)) {
+    cout << "Harvest results incorrect!" << endl;
+    return;
+  }
+  cout << "All harvest tests passed!" << endl;
+  // delete
+  delete Potato;
+  delete p1;
 }
 
 int main(void) {
-    //Run Tests
-    plant_constructor_test();
-    plant_set_method_tests();
-    plant_waterMethods_tests();
+  // Run Tests
+  plant_constructor_test();
+  plant_set_method_tests();
+  plant_waterMethods_tests();
+  plant_advanceDay_test();
+  plant_harvest_test();
 }
