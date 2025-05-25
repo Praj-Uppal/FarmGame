8dec8b64c4482d6203b37ab7eab65ed886e177c6
Praj-Uppal <a1925661@adelaide.edu.au>
2025-05-25 14:03:58 +0930

    Formatting

--- src/CarrotPlant.h
+++ src/CarrotPlant.h
@@ -1,17 +1,18 @@
 #ifndef CARROTPLANT_H
 #define CARROTPLANT_H
 
-#include "Item.h"
-#include "Plant.h"
 #include <vector>
 
+#include "Item.h"
+#include "Plant.h"
+
 class CarrotPlant : public Plant {
-    public:
-    //Declare constructor
-        CarrotPlant(tuple<int, int> position);
-        //Declare advance day functions and Harvest function
-        void advanceDay();
-        vector<tuple<Item*,int>> harvest();
+ public:
+  // Declare constructor
+  CarrotPlant(tuple<int, int> position);
+  // Declare advance day functions and Harvest function
+  void advanceDay();
+  vector<tuple<Item*, int>> harvest();
 };
 
 #endif
